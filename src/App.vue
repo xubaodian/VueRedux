63d798adf1e75d5fecedede4c3f68d54cc094214
xubaodian <xubaodian@hikvision.com.cn>
2019-02-01 14:01:28 +0800

    create project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      str: '12341234',
+      inputText: '',
+      searchList: [
+        '1231',
+        '测试时'
+      ]
+    };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+#app {
+  background: url("./assets/bk.jpg") no-repeat;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import WsltUI from 'wstl-ui';
+
+import 'wstl-ui/lib/index.css';
+
+Vue.use(WsltUI);
+
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
